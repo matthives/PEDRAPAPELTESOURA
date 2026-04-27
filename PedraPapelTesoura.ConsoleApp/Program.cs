aef2f231bd5287600c9321652aa6bd6d7f9ff00b
Mateus Thives <mateus.thives@hotmail.com>
2026-04-27 19:30:08 -0300

    Atualizada opções de execução

--- PedraPapelTesoura.ConsoleApp/Program.cs
+++ PedraPapelTesoura.ConsoleApp/Program.cs
@@ -16,8 +16,8 @@ while (true)
         Console.WriteLine("--------------------------");
         Console.WriteLine("Pedra, Papel, Tesoura");
         Console.WriteLine("--------------------------");
-        Console.WriteLine("1 - Tesoura");
-        Console.WriteLine("2 - Tesoura");
+        Console.WriteLine("1 - Pedra");
+        Console.WriteLine("2 - Papel");
         Console.WriteLine("3 - Tesoura");
         Console.WriteLine("--------------------------");
 
